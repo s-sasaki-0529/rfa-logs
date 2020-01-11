e3e16eca3c2278520254ead4f58468d08f1b7606
Shingo.sasaki <shingo.sasaki@studist.jp>
2020-01-11 17:43:08 +0900

    rfa関数から全てを始めるように

---
+++ functions/rfa.js
@@ -0,0 +1,16 @@
+const aws = require('aws-sdk')
+const lambda = new aws.Lambda()
+
+module.exports.index = async event => {
+  // TODO: Twitterをチェックするlambda経由で最新の画像情報を取得する
+  const { url, dateTime, userName } = event
+
+  const result = await lambda.invoke({
+    FunctionName: 'sasaki-rfa-logs-dev-imageCreator',
+    Payload: JSON.stringify({url, dateTime, userName}) // pass params
+  }).promise()
+
+  return {
+    statusCode: 200
+  }
+}
