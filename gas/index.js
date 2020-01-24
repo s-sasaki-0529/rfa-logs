5d4dfbc144b6857d7e89cf51cb92773b7929ab23
Shingo.sasaki <shingo.sasaki@studist.jp>
2020-01-25 03:11:28 +0900

    スプレッドシートの構造を大幅変更　”

--- gas/index.js
+++ gas/index.js
@@ -1,6 +1,18 @@
+var COL_SKILL_NAME = 1
+var COL_TYPE       = 2
+var COL_UNIT       = 3
+var COL_RESULT     = 4
+var COL_TOTAL      = 5
+var COL_TARGET     = 6
+var COL_GOAL       = 7
+var COL_PROGRESS   = 8
+var COL_UPDATED    = 9
+var COL_COMPLETED  = 10
+
 var sheet = SpreadsheetApp.getActiveSheet();
 
 function searchRow(text) {
+  Logger.log("searched: " + text);
   var textFinder = sheet.createTextFinder(text).matchEntireCell(true);
   var range = textFinder.findNext();
   return range.getRow();
@@ -20,20 +32,27 @@ function updateResults() {
     var total = results[key].value;
     var updatedAt = new Date(results[key].updatedAt);
     var row = searchRow(name)
-    sheet.getRange(row, 2).setValue(total);
-    sheet.getRange(row, 6).setValue(updatedAt);
+    Logger.log(name)
+    sheet.getRange(row, COL_TOTAL).setValue(total);
+    sheet.getRange(row, COL_UPDATED).setValue(updatedAt);
   })
 }
 
 function updateBackgroundColor() {
-  var rowsCount = 82;
+  var rowsCount = 84;
   for (var row = 2; row <= rowsCount; row++) {
-    var nextTarget = sheet.getRange(row, 4).getValue()
-    var columns = sheet.getRange(row, 1, 1, 6)
-    if (nextTarget === '') {
-      columns.setBackgroundRGB(220, 220, 220)
-    } else if (nextTarget <= 0) {
-      columns.setBackgroundRGB(255, 228, 225)
+    var type = sheet.getRange(row, COL_TYPE).getValue()
+    var columns = sheet.getRange(row, 1, 1, COL_COMPLETED)
+    if (type == 'うで') {
+      columns.setBackgroundRGB(255, 173, 153)
+    } else if (type == 'はら') {
+      columns.setBackgroundRGB(255, 255, 179)
+    } else if (type == 'あし') {
+      columns.setBackgroundRGB(179, 179, 255)
+    } else if (type == 'ヨガ') {
+      columns.setBackgroundRGB(179, 255, 179)
+    } else if (type == '回復') {
+      columns.setBackgroundRGB(255, 179, 255)
     } else {
       columns.setBackgroundRGB(255, 255, 255)
     }
