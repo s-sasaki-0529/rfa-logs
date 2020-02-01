ab644f571c3b8f80217d0fda50bb212833d737e6
Shingo.sasaki <shingo.sasaki@studist.jp>
2020-02-02 02:52:16 +0900

    オリジナルのURLをDynamoに入れる

--- functions/observer.js
+++ functions/observer.js
@@ -1,14 +1,45 @@
 const aws = require('aws-sdk')
 const lambda = new aws.Lambda()
+const dynamoClient = new aws.DynamoDB.DocumentClient({region: 'ap-northeast-1'})
+
+async function fetchLogs({userName}) {
+  const params = {
+    TableName: 'rfa-log-results',
+    Key: { userName }
+  }
+  const currentDoc = await dynamoClient.get(params).promise()
+  if (currentDoc.hasOwnProperty('Item')) {
+    return currentDoc.Item.urls
+  } else {
+    return {}
+  }
+}
+
+async function insertLog({userName, url}) {
+  const newUrls = await fetchLogs( { userName })
+  newUrls.push(url)
+
+  const newDoc = await dynamoClient.update({
+    TableName: 'rfa-log-results',
+    Key: { userName },
+    UpdateExpression: 'set urls = :u',
+    ExpressionAttributeValues: {
+      ':u': newUrls
+    },
+    ReturnValues: 'UPDATED_NEW'
+  }).promise()
+  return newDoc
+}
 
 module.exports.index = async event => {
   // TODO: Twitterをチェックするlambda経由で最新の画像情報を取得する
-  const { url, dateTime, userName } = event
-
+  const { url, userName } = event
   const result = await lambda.invoke({
     FunctionName: 'sasaki-rfa-logs-dev-imageCreator',
     Payload: JSON.stringify({url, userName}) // pass params
   }).promise()
+
+  await insertLog({ userName, url })
 
   return {
     statusCode: 200
