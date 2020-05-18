3497e548fcca5f98d75769ea7fdec8a9076f243c
Shingo.sasaki <shingo.sasaki@studist.jp>
2020-05-18 14:40:51 +0900

    Prettier適用

--- functions/dbWriter.js
+++ functions/dbWriter.js
@@ -1,59 +1,63 @@
-const aws = require('aws-sdk')
-const dynamoClient = new aws.DynamoDB.DocumentClient({region: 'ap-northeast-1'})
+const aws = require("aws-sdk");
+const dynamoClient = new aws.DynamoDB.DocumentClient({
+  region: "ap-northeast-1",
+});
 
 function originUrl(url) {
-  return url.substr(0, url.lastIndexOf('/')) + '/origin.jpeg'
+  return url.substr(0, url.lastIndexOf("/")) + "/origin.jpeg";
 }
 
-function mergeResults({currentResults, newResults}) {
-  const mergedResults = {}
+function mergeResults({ currentResults, newResults }) {
+  const mergedResults = {};
 
   Object.keys(currentResults).forEach((key) => {
     if (newResults[key] && newResults[key].value >= currentResults[key].value) {
-      console.log(`更新: ${newResults[key]}`)
-      mergedResults[key] = newResults[key]
+      console.log(`更新: ${newResults[key]}`);
+      mergedResults[key] = newResults[key];
     }
-  })
+  });
 
-  return mergedResults
+  return mergedResults;
 }
 
-async function fetchCurrentResult({userName}) {
+async function fetchCurrentResult({ userName }) {
   const params = {
-    TableName: 'rfa-logs',
-    Key: { userName }
-  }
-  const currentDoc = await dynamoClient.get(params).promise()
-  if (currentDoc.hasOwnProperty('Item')) {
-    return currentDoc.Item.results
-  } else {
-    return {}
-  }
-}
-
-async function updateResult({userName, imageUrl, results}) {
-  const currentResults = await fetchCurrentResult({ userName })
-  const mergedResults = mergeResults({ currentResults, newResults: results})
-  console.log({ mergedResults })
-
-  const newDoc = await dynamoClient.update({
-    TableName: 'rfa-logs',
+    TableName: "rfa-logs",
     Key: { userName },
-    UpdateExpression: 'set results = :r',
-    ExpressionAttributeValues: {
-      ':r': {
-        ...currentResults,
-        ...mergedResults
-      }
-    },
-    ReturnValues: 'UPDATED_NEW'
-  }).promise()
-  return newDoc
+  };
+  const currentDoc = await dynamoClient.get(params).promise();
+  if (currentDoc.hasOwnProperty("Item")) {
+    return currentDoc.Item.results;
+  } else {
+    return {};
+  }
 }
 
-module.exports.index = async event => {
-  const { userName, imageUrl, results } = event
-  console.log({results})
+async function updateResult({ userName, imageUrl, results }) {
+  const currentResults = await fetchCurrentResult({ userName });
+  const mergedResults = mergeResults({ currentResults, newResults: results });
+  console.log({ mergedResults });
 
-  await updateResult({ userName, imageUrl, results })
+  const newDoc = await dynamoClient
+    .update({
+      TableName: "rfa-logs",
+      Key: { userName },
+      UpdateExpression: "set results = :r",
+      ExpressionAttributeValues: {
+        ":r": {
+          ...currentResults,
+          ...mergedResults,
+        },
+      },
+      ReturnValues: "UPDATED_NEW",
+    })
+    .promise();
+  return newDoc;
 }
+
+module.exports.index = async (event) => {
+  const { userName, imageUrl, results } = event;
+  console.log({ results });
+
+  await updateResult({ userName, imageUrl, results });
+};
